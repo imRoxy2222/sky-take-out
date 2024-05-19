a4ecef72dfa6972bbebc579aacbc2d637b591294
Roxy2222 <frank2222@foxmail.com>
2024-05-19 20:07:46 +0800

    增加 添加员工接口

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -12,16 +15,35 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
-
-    /**
-     * 捕获业务异常
-     * @param ex
-     * @return
-     */
-    @ExceptionHandler
-    public Result exceptionHandler(BaseException ex){
-        log.error("异常信息：{}", ex.getMessage());
-        return Result.error(ex.getMessage());
-    }
-
+	
+	/**
+	 * 捕获业务异常
+	 *
+	 * @param ex
+	 * @return Result<String>
+	 */
+	@ExceptionHandler
+	public Result<String> exceptionHandler(BaseException ex) {
+		log.error("异常信息：{}", ex.getMessage());
+		return Result.error(ex.getMessage());
+	}
+	
+	/**
+	 * Mysql异常
+	 *
+	 * @param ex
+	 * @return
+	 */
+	@ExceptionHandler
+	public Result<String> exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+		String messgage = ex.getMessage();
+		// Duplicate entry '511621200309202359' for key 'idx_username'
+		if (messgage.contains("Duplicate")) {
+			String username = messgage.split(" ")[2];
+			return Result.error(username + MessageConstant.ALREADY_EXIST);
+		} else {
+			return Result.error(MessageConstant.UNKNOWN_ERROR);
+		}
+		
+	}
 }
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -5,14 +5,18 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
-public interface EmployeeMapper {
+public interface EmployeeMapper{
+	
+	/**
+	 * 根据用户名查询员工
+	 *
+	 * @param username
+	 * @return
+	 */
+	@Select("select * from employee where username = #{username}")
+	Employee getByUsername(String username);
+	
 
-    /**
-     * 根据用户名查询员工
-     * @param username
-     * @return
-     */
-    @Select("select * from employee where username = #{username}")
-    Employee getByUsername(String username);
-
+	
+	void insert(Employee employee);
 }
